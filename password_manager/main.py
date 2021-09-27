98eabd93bb50ce2a8f08bac50c9015d7a4bf2373
filipe casa <filipecasa@gmail.com>
2021-09-27 13:44:02 +0200

    functioning password manager

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,46 +1,90 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+#Password Generator Project
+def generate_password():
+
+    password_entry.delete(0, END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save():
-    data_to_save = f"{website_entry} | {email_entry} | {password_entry}"
-    with open("data.txt", "a") as data:
-        data.write(data_to_save)
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+
+    if website == "" or password == "":
+    # if len(website) == 0
+        messagebox.askokcancel(title="Oops", message="Please don't leave any fields empty!")
+    # messagebox.showinfo(title="Title", message="Message")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\n"
+                                                              f"Passoword: {password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data:
+                data.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = tk.Tk()
+window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = tk.Canvas(width=200, height=200)
-locker_img = tk.PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+locker_img = PhotoImage(file="logo.png")
 
 canvas.create_image(100, 110, image=locker_img)
 canvas.grid(column=1, row=0)
 
 # Labels
-website_label = tk.Label(text="Website:")
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-email_label = tk.Label(text="Email/Username:")
+email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-password_label = tk.Label(text="Password:")
+password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = tk.Entry(width=53)
+website_entry = Entry(width=53)
 website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
-email_entry = tk.Entry(width=53)
+email_entry = Entry(width=53)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, "your@email.com")
-password_entry = tk.Entry(width=34)
+password_entry = Entry(width=34)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_password_button = tk.Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
-add_button = tk.Button(text="Add", width=45, command=save)
+add_button = Button(text="Add", width=45, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
