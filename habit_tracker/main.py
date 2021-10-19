88747e0d679b0dbaf661f80794e5d6957829fa1e
filipe casa <filipecasa@gmail.com>
2021-10-19 18:02:24 +0200

    Add a pixel to your graph and description added

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -2,6 +2,7 @@ import requests
 
 USERNAME = "your username"
 TOKEN = "your token"
+GRAPH_ID = "your graph id"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -12,13 +13,14 @@ user_params = {
     "notMinor": "yes",
 }
 
+# Create an account in Pixe.la
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "Km",
     "type": "float",
@@ -29,7 +31,16 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+# Create a graph in Pixe.la
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+pixel_config = {
+    "date": "date",
+    "quantity": "amount of km as a float ex: 1.1",
+}
 
+# Add a pixel to your graph in Pixe.la
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
