4961f34db8b8df3593d45848cb4f9526e4b7da2e
filipe casa <filipecasa@gmail.com>
2021-10-20 17:17:11 +0200

    How to update and delete pixels

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -38,15 +38,36 @@ headers = {
 
 pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
-today = datetime.now()
+today = datetime.now().strftime("%Y%m%d")
 # print(today.strftime("%Y%m%d"))
 distance_km = 1.1 # amount of km as a float ex: 1.1"
 
 pixel_config = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": f"{distance_km}",
+    "date": today,
+    "quantity": input("How many kilometers did you cycle today?: "),
 }
 
 # Add a pixel to your graph in Pixe.la
-# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+print(response.text)
+
+date_to_update = today
+new_distance_km = "km to be updated"
+
+update_pixel_endpoint = f"{pixel_endpoint}/{date_to_update}"
+
+update_pixel_config = {
+    "quantity": f"{new_distance_km}",
+}
+
+# Update a pixel
+# response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+# print(response.text)
+
+date_to_delete = "date of pixel to be deleted"
+
+delete_pixel_endpoint = f"{pixel_endpoint}/{date_to_delete}"
+
+# Delete a pixel
+# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
 # print(response.text)
